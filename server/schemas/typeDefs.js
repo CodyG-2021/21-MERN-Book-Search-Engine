5056e4b2e1724ebbca8125bc4bcda30286cdc074
Cody <gallagher2021@gmail.com>
2021-07-17 20:01:26 -0600

    Got the signup and login working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,47 +1,43 @@
+// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Auth {
-    token: ID
-    user: User
+type Book {
+    _id: ID!
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
   }
-
-  type User {
-    _id: ID
+type User {
+    _id: ID!
     username: String
     email: String
     bookCount: Int
     savedBooks: [Book]
   }
-
-  type Book {
-		_id: ID
-    bookId: String
-    authors: [String]
-    description: String
-    image: String
-    link: String
-    title: String
-  }
-
-  input BookInput {
-		_id: ID
-    bookId: String
-    authors: [String]
+input savedBook {
     description: String
     title: String
+    bookId: String
     image: String
     link: String
-  } 
-
-  type Query {
-    me: User
+    authors: [String]
+}
+type Query {
+    me: User  
   }
-  type Mutation {
+type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(book: BookInput!): User
-    removeBook(bookId: String!): User
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+}
+type Auth {
+    token: ID!
+    user: User
   }
 `;
 
